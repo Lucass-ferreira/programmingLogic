8a2be44e6dfccd32cdaf2fd388e79ddfe8f4fa63
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2025-08-04 15:52:23 -0300

    exercicio baskara

---
+++ cap04_estruturaCondicional/exercicios/ex_baskara.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double coeficiente_a, coeficiente_b, coeficiente_c, x1, x2, delta;
+
+    printf("Coeficiente a: ");
+    scanf("%lf", &coeficiente_a);
+
+    printf("Coeficiente b: ");
+    scanf("%lf", &coeficiente_b);
+
+    printf("Coeficiente c: ");
+    scanf("%lf", &coeficiente_c);
+
+    //delta não poder ser zero, esta equação não possui raizes reais
+    //coeficiente a não pode ser 0
+
+    delta = (pow(coeficiente_b, 2)) - (4 * coeficiente_a * coeficiente_c);
+
+    if(coeficiente_a == 0 || delta < 0){
+        printf("esta equacao nao possui raizes reais");
+    } else {
+        x1 = (-coeficiente_b + sqrt(delta)) / (2 * coeficiente_a);
+        x2 = (-coeficiente_b - sqrt(delta)) / (2 * coeficiente_a);
+    }
+
+    //printf("%.4f", delta);
+    printf("X1 = %.4f \n", x1);
+    printf("X2 = %.4f \n", x2);
+
+    return 0;
+}
