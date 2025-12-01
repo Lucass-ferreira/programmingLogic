a4a5d5ac09761c8800870a5aec377c10efed3603
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2025-12-01 15:15:08 -0300

    exercício crescente

---
+++ cap05_estruturasRepetitivas/execicios/ex_crescente.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2;
+
+    printf("Digite dois numeros: \n");
+    scanf("\n %d", &num1);
+    scanf("%d", &num2);
+
+    while (num1 != num2){
+
+        if(num1 < num2){
+            printf("CRESCENTE! \n");
+        } else {
+            printf("DECRESCENTE! \n");
+        }
+
+        printf("Digite outros dois numeros: \n");
+        scanf("\n %d", &num1);
+        scanf("%d", &num2);
+    }
+    
+    return 0;
+}
